36ab186e3e58974ba1f78f7cb19f8cdbc1e71195
davytry <96565721+davytry@users.noreply.github.com>
2022-04-19 19:42:08 +0800

    test

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -5,7 +5,6 @@ package com.atguigu.git;
  */
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello!git");
         System.out.println("hello!git!");
         System.out.println("master");
         System.out.println("hot-fix");
