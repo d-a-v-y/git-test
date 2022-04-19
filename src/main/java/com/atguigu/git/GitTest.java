1a60fa58badf1506d663fa34e7e0c53458615dcf
Davy <2689022556@qq.com>
2022-04-19 19:45:19 +0800

    push commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("hello!git!");
         System.out.println("master");
         System.out.println("hot-fix");
+        System.out.println("push");
     }
 }
